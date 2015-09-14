3c70c9e3b2425fe25cd96a8ea32e548d70754bfa
Pierre-Victor Chaumier <chaumierpv@gmail.com>
2015-09-14 00:26:35 -0300

    Add first homework. Recognition of time from images.

---
+++ T1/image.py
@@ -0,0 +1,302 @@
+"""Image processing library.
+
+    author: Pierre-Victor Chaumier <chaumierpv@gmail.com>
+
+    Python Version : 3.4.2
+
+    Required libraries :
+        - Pillow        v2.9.0
+        - scipy         v0.16.0
+        - numpy         v1.9.2
+        - matplotlib    v1.4.3
+
+    RQ : in all the fonctions, we assume that the images are rectangular.
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics as stat
+
+from collections import Counter
+from scipy import misc
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+## -------------------- Display image and characteristics  -------------------
+
+def plot(image, title="", threshold=256):
+    """Creates a new figure and display the image given in argument.
+
+    The function also works with a 2D list.
+    
+    The threshold does not actually modify the given image. If you want to do
+    so, you need to use the threshold function defined later.
+
+    """
+
+    plt.figure()
+    # There is a threshold included in the imshow function
+    if threshold == 256:
+        plt.imshow(image)
+    else:
+        plt.imshow(image > threshold)
+    # pyplot uses thermal color by default, next line enables display
+    # in black/white
+    plt.gray()
+    if title != "":
+        plt.title(title)
+
+def draw_circle(fig, center, radius, color='red'):
+    """Add a circle to current figure."""
+    circle = plt.Circle(center, radius, fill=False, color=color)
+    fig.gca().add_artist(circle)
+
+def display_monochrome_3D(image):
+    """Display the 3D representation of the image (position and gray value).
+
+    The levels of gray are represented by temperature colors
+
+    """
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    X = np.arange(0, image.shape[0], 1)
+    Y = np.arange(0, image.shape[1], 1)
+    X, Y = np.meshgrid(X, Y)
+    surf = ax.plot_surface(X, Y, image, cmap=cm.coolwarm, linewidth=0)
+    plt.title('3D representation of image')
+
+
+def freq(image, nb_of_level=256, title='', xlabel='Level'):
+    """Display the historiogram frequency/value."""
+
+    plt.figure()
+    plt.hist(image.flatten(), nb_of_level)
+    plt.xlabel(xlabel)
+    plt.ylabel('Frequency')
+    plt.title(title)
+
+def freq_size(image_labelled):
+    """Display the graph frequency of size/size of regions."""
+    c = determine_size_of_regions(image_labelled)
+    # The biggest area is the black one, so we delete it
+    del c[c.most_common(1)[0][0]]
+    sizes = []
+    for el in c:
+        sizes.append(c[el])
+    plt.figure()
+    plt.hist(sizes)
+
+## -------------------- Transformation of image  -------------------
+
+def rgb_2_gray(image):
+    """Returns a new image in gray using the CIE RGB results.
+
+        gray_level = 1.0000 * R + 4.5907 * G + 0.0601 * B
+    
+    https://en.wikipedia.org/wiki/CIE_1931_color_space
+    The functions do not take into account the level of transparency.
+
+    """
+
+    img_gray = np.ndarray((image.shape[0], image.shape[1]))
+    R = image[:, :, 0]
+    G = image[:, :, 1]
+    B = image[:, :, 2]
+    img_gray[:, :] = (1.0000 * R + 4.5907 * G + 0.0601 * B) / 5.6508
+    return img_gray
+
+
+def rgb_2_monochrome(image, color):
+    """Returns a new image in the color specified.
+
+    The different possible color arguments are R, G or B.
+    We assume we work on square images.
+
+    """
+    
+    if color == 'R':
+        return image[:, :, 0]
+    elif color == 'G':
+        return image[:, :, 1]
+    elif color == 'B':
+        return image[:, :, 2]
+
+
+def threshold(image, threshold, threshold_end=255):
+    """Returns a new numpy array which is a thresholded version of the given 
+    one.
+
+    Note that you can select an interval by specifying the threshold_end.
+
+    /!\ The image given must be a monochrome (2D numpy array).
+    
+    """
+
+    image_thresholded = np.copy(image)
+    if threshold_end != 255:
+        image_thresholded[
+                (image_thresholded > threshold_end) |
+                (image_thresholded < threshold)] = 0
+        image_thresholded[
+                (image_thresholded <= threshold_end) &
+                (image_thresholded >= threshold)] = 255
+    else:
+        image_thresholded[image_thresholded < threshold] = 0
+        image_thresholded[image_thresholded >= threshold] = 255
+    return image_thresholded
+
+## -------------------- Cleaning of image  -------------------
+
+def background_elimination(image):
+    """Returns an image with a normalized background.
+
+    Works by going row by row and substracting the minimum of each row to each
+    pixel of the row.
+
+    This shifts the values towards the black.
+    """
+    image_bg_eliminated = np.copy(image)
+    for i in range(image.shape[0]):
+        mini = min(image[i])
+        for j in range(image.shape[1]):
+            image_bg_eliminated[i][j] = image[i][j] - mini
+    return image_bg_eliminated
+
+
+def add_equivalent_labels(equivalent_list, left_label, top_label):
+    """Add an equivalence between two labels. 
+
+    It also takes care of merging two subset that are equivent.
+    """
+    pos_left_label = -1
+    pos_top_label = -1
+    for i, el in enumerate(equivalent_list):
+        if left_label in el:
+            pos_left_label = i
+        if top_label in el:
+            pos_top_label = i
+    # None of the two label already present
+    if pos_left_label == -1 and pos_top_label == -1:
+        equivalent_list.append(set([left_label, top_label]))
+    # One of the two label is already in one sub-ensemble
+    elif pos_left_label == -1 and pos_top_label != -1:
+        equivalent_list[pos_top_label].add(left_label)
+    elif pos_left_label != -1 and pos_top_label == -1:
+        equivalent_list[pos_left_label].add(top_label)
+    # Both are present but in different label sets
+    elif pos_top_label != pos_left_label:
+        equivalent_list[pos_top_label] = equivalent_list[pos_top_label].union(
+                                             equivalent_list[pos_left_label])
+        del equivalent_list[pos_left_label]
+
+def renumber_region(image_labelled):
+    # Now number the region from 1 to 1
+    new_labels = dict()
+    current_new_label = 1
+    for i in range(image_labelled.shape[0]):
+        for j in range(image_labelled.shape[1]):
+            if image_labelled[i][j] not in new_labels and \
+               image_labelled[i][j] != 0:
+                new_labels[image_labelled[i][j]] = current_new_label
+                current_new_label += 1
+    for i in range(image_labelled.shape[0]):
+        for j in range(image_labelled.shape[1]):
+            if image_labelled[i][j] != 0:
+                image_labelled[i][j] = new_labels[image_labelled[i][j]]
+
+def label_region(image):
+    """Labels all region in the binary image.
+
+    The algorithm starts by checking if the current pixel is white.
+
+    If so, it looks if the above and/or left pixel is white also
+    and use their label to label the current pixel. If the top and left pixels
+    are labelled but with a different label, it adds both the labels to an
+    equivalent_list (which type is a list of sets).
+
+    If they are not marked, it will create a new label and label it.
+
+        - x -
+        x o - 
+        - - -
+
+    o = current pixel
+    x = top and left pixels
+    - = pixels not considered
+    """
+
+    image_labelled = np.ndarray((image.shape[0], image.shape[1]))
+    current_label = 0
+    equivalent_list = []
+    # i is the height and j the width
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            # if the pixel is not black
+            if image[i][j] != 0:
+                # Edge cases of the position in the image
+                # First pixel (0, 0)
+                if i == 0 and j == 0:
+                    current_label += 1
+                    image_labelled[i][j] = current_label
+                # On the first line (x, 0)
+                elif i != 0 and j == 0:
+                    if image[i - 1][j] != 0:
+                        image_labelled[i][j] = image_labelled[i - 1][j]
+                    else:
+                        current_label += 1
+                        image_labelled[i][j] = current_label
+                # On the first column (0, x)
+                elif i == 0 and j != 0:
+                    if image[i][j - 1] != 0:
+                        image_labelled[i][j] = image_labelled[i][j - 1]
+                    else:
+                        current_label += 1
+                        image_labelled[i][j] = current_label
+                else:
+                    if image[i][j - 1] != 0:
+                        # We use top label by default
+                        image_labelled[i][j] = image_labelled[i][j - 1]
+                        if image[i - 1][j] != 0 and \
+                           image_labelled[i - 1][j] != image_labelled[i][j - 1]:
+                            add_equivalent_labels(equivalent_list, 
+                                                  image_labelled[i - 1][j],
+                                                  image_labelled[i][j - 1])
+                    elif image[i - 1][j] != 0:
+                        image_labelled[i][j] = image_labelled[i - 1][j]
+                    else:
+                        current_label += 1
+                        image_labelled[i][j] = current_label
+                    
+    renumber_region(image_labelled)
+
+    # keep one example of each label class
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            for el in equivalent_list:
+                if image_labelled[i][j] in el:
+                    image_labelled[i][j] = min(el)
+
+
+
+    return image_labelled
+
+def determine_size_of_regions(image_labelled):
+    """Count how many pixel each regions has. Returns a Counter object."""
+    c = Counter()
+    for el in image_labelled:
+        c += Counter(el)
+    return c
+
+def delete_small_region(image_labelled, size):
+    """Remove areas of size inferior or equal to the size argument."""
+    c = determine_size_of_regions(image_labelled)
+    for i, row in enumerate(image_labelled):
+        for j, pixel in enumerate(row):
+            if pixel != 0:
+                if c[pixel] <= size:
+                    image_labelled[i][j] = 0
+    renumber_region(image_labelled)
+
+
---
+++ T1/tarea1.py
@@ -0,0 +1,384 @@
+# encoding: utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics as stat
+
+from collections import Counter
+from scipy import misc
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+import image
+
+
+## --------------------  1. Looking for the center  -------------------
+
+def threshold_image(img):
+    """We start by separating from white to black.
+
+    As all images do not have the same repartition of level of grey
+    we need to use a variating threshold.
+    
+    """
+    img_gray = image.rgb_2_gray(img)
+    threshold = np.mean(img_gray)
+    img_gray_thresholded = image.threshold(img_gray, threshold - 15)
+    return img_gray_thresholded
+
+def clean_thresholded_image(img_thresholded, precision):
+    """deletion of black column/rows (like the right column in the image3)."""
+    for i in range(img_thresholded.shape[0]):
+        if img_thresholded[i].sum() \
+                <= 1 * precision * img_thresholded.shape[0]:
+            img_thresholded[i, :] = 255
+    for i in range(img_thresholded.shape[1]):
+        if img_thresholded[:, i].sum() \
+                <= 1 * precision * img_thresholded.shape[1]:
+            img_thresholded[:, i] = 255
+
+
+def find_center_radius(img_gray_thresholded, precision):
+    """Returns a tuple with the coordonate of the center of the clock and the
+    radius."""
+
+    #
+    # We try to find the first lines/rows that contains at least precision%
+    # of black pixel from the left/right/top/down.
+    #
+    top = 0
+    while top < img_gray_thresholded.shape[0] and \
+          img_gray_thresholded[top].sum() \
+                > 255 * precision * img_gray_thresholded.shape[0]:
+        top += 1
+    down = img_gray_thresholded.shape[0] - 1
+    while down > 0 and \
+          img_gray_thresholded[down].sum() \
+            > 255 * precision * img_gray_thresholded.shape[0]:
+        down -= 1
+    left = 0
+    while left < img_gray_thresholded.shape[1] and \
+          img_gray_thresholded[:, left].sum() > 255 * precision * img_gray_thresholded.shape[1]:
+        left += 1
+    right = img_gray_thresholded.shape[1] - 1
+    while right > 0 and \
+          img_gray_thresholded[:, right].sum() > 255 * precision * img_gray_thresholded.shape[1]:
+        right -= 1
+
+    #
+    #   Calculating the center and radius
+    #
+
+    center = ((top + down) / 2, (left + right) / 2)
+    radius = (left - right) / 2
+    
+    #
+    #   Display the centers
+    #
+
+    # # Uncomment following line to display
+    # fig = plt.figure()
+    # plt.imshow(img_gray_thresholded)
+    # plt.gray()
+    # plt.plot([center[1], center[1]], [0, img_gray_thresholded.shape[1]], color='red', linestyle='--')
+    # plt.plot([0, img_gray_thresholded.shape[0]], [center[0], center[0]], color='red', linestyle='--')
+    # plt.plot([left, left], [0, img_gray_thresholded.shape[1]], color='red', linestyle='--')
+    # plt.plot([right, right], [0, img_gray_thresholded.shape[1]], color='red', linestyle='--')
+    # plt.plot([0, img_gray_thresholded.shape[0]], [top, top], color='red', linestyle='--')
+    # plt.plot([0, img_gray_thresholded.shape[0]], [down, down], color='red', linestyle='--')
+    
+    return center, radius
+
+
+## --------------------  Finding the time  -------------------
+
+def only_red(img):
+    """Transform the image to keep the parts that contains two times more
+    red than green.
+    
+    As the green and blue levels are equal in the images given, 
+    it is not necessary to use red > green + blue to threshold.
+
+    """
+    
+    red = np.ndarray((img.shape[0], img.shape[1]))
+    red.fill(0)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j][0] > 2 * img[i][j][1]:
+                red[i][j] = 255
+    return red
+
+def angle_to_unit(avg_angle, nb_of_unit):
+    """Transform a given angle to a unit number."""
+
+    unit_float = nb_of_unit * avg_angle / (2 * np.pi)
+    unit = int(unit_float)
+    if unit_float - unit > 0.5:
+        return unit + 1
+    else:
+        return unit
+
+def time(img, center, radius):
+    """Find the time given the image of a clock, its center and radius."""
+
+    time = [0, 0, 0]
+
+    #
+    #   Defining the circles for detection.
+    #
+
+    # 600 tetas
+    teta = np.arange(0, 2 * np.pi, np.pi / 300)
+    # Small circle
+    x_s_circle = (radius / 2) * np.cos(teta) + center[0]
+    y_s_circle = (radius / 2) * np.sin(teta) + center[1]
+    # Large circle
+    x_l_circle = (radius / 1.5) * np.cos(teta) + center[0]
+    y_l_circle = (radius / 1.5) * np.sin(teta) + center[1]
+
+    #
+    #   Detect seconds
+    #
+
+    begin_sec = 0
+    end_sec = 0
+    i = 0
+
+    # Detect first and last pixel
+    img_only_red = only_red(img)
+
+    if img_only_red[x_l_circle[i]][y_l_circle[i]] == 0:
+        while i < len(x_l_circle) and \
+              img_only_red[x_l_circle[i]][y_l_circle[i]] == 0:
+            i += 1
+        begin_sec = i
+        end_sec = begin_sec
+        while i < len(x_l_circle) and \
+              img_only_red[x_l_circle[i]][y_l_circle[i]] != 0:
+            i += 1
+        end_sec = i
+
+    # Changing angle to secs
+    avg_sec = int((end_sec + begin_sec) / 2)
+    teta_sec_avg = 2 * np.pi - teta[avg_sec]
+    time[2] = angle_to_unit(teta_sec_avg, 60)
+
+    #
+    #   Detect minutes
+    #
+
+    begin_min = 0
+    end_min = 0
+    i = 0
+
+    img_gray_thresholded = threshold_image(img)
+
+    # Changin begin_sec and end_sec to correspond to the thresholded graph
+    end_sec = (begin_sec + end_sec) // 2
+    while img_gray_thresholded[x_l_circle[end_sec]][y_l_circle[end_sec]] == 0:
+        end_sec += 1
+    begin_sec = (begin_sec + end_sec) // 2
+    while img_gray_thresholded[x_l_circle[begin_sec]][y_l_circle[begin_sec]] == 0:
+        begin_sec -= 1
+
+    # Detecting begin and end of minute bar
+    if img_gray_thresholded[x_l_circle[i]][y_l_circle[i]] != 0:
+        while i < len(x_l_circle) and \
+              (img_gray_thresholded[x_l_circle[i]][y_l_circle[i]] != 0 or
+              i in range(begin_sec, end_sec + 1)):
+            i += 1
+        begin_min = i
+        # Test if the sec and minute bar are not superimposed
+        if begin_min != 600:
+            end_min = begin_min
+            while i < len(x_l_circle) and \
+                  img_gray_thresholded[x_l_circle[i]][y_l_circle[i]] == 0:
+                i += 1
+            end_min = i
+
+            # Changing angle to min
+            avg_min = int((end_min + begin_min) / 2)
+            teta_min_avg = 2 * np.pi - teta[avg_min]
+            time[1] = angle_to_unit(teta_min_avg, 60)
+        else:
+            begin_min, end_min = begin_sec, end_sec
+            avg_min = int((end_min + begin_min) / 2)
+            avg_min = 2 * avg_min - avg_sec
+            teta_min_avg = 2 * np.pi - teta[avg_min]
+            time[1] = angle_to_unit(teta_min_avg, 60)
+
+    #
+    #   Detect hours
+    #
+
+    begin_hour = 0
+    end_hour = 0
+    i = 0
+
+    # Changin begin_sec, end_sec, begin_min and end_min to correspond
+    # to the thresholded graph and the small circle.
+    end_sec = (begin_sec + end_sec) // 2
+    while img_gray_thresholded[x_s_circle[end_sec]][y_s_circle[end_sec]] == 0:
+        end_sec += 1
+    begin_sec = (begin_sec + end_sec) // 2
+    while img_gray_thresholded[x_s_circle[begin_sec]][y_s_circle[begin_sec]] == 0:
+        begin_sec -= 1
+
+    end_min = (begin_min + end_min) // 2
+    while img_gray_thresholded[x_s_circle[end_min]][y_s_circle[end_min]] == 0:
+        end_min += 1
+    begin_min = (begin_min + end_min) // 2
+    while img_gray_thresholded[x_s_circle[begin_min]][y_s_circle[begin_min]] == 0:
+        begin_min -= 1
+
+    # Detecting hour bar
+    if img_gray_thresholded[x_s_circle[i]][y_s_circle[i]] != 0:
+        while i < len(x_s_circle) and \
+              (img_gray_thresholded[x_s_circle[i]][y_s_circle[i]] != 0 or
+              i in range(begin_sec, end_sec) or
+              i in range(begin_min, end_min)):
+            i += 1
+        begin_hour = i
+        # Test if the hour bar is not on top of minute and/or sec bar.
+        if begin_hour != 600:
+            end_hour = begin_hour
+            while i < len(x_s_circle) and \
+                  img_gray_thresholded[x_s_circle[i]][y_s_circle[i]] == 0:
+                i += 1
+            end_hour = i
+        
+            # Changing angle to hours
+            avg = int((end_hour + begin_hour) / 2)
+            teta_hour_avg = 2 * np.pi - teta[avg]
+            time[0] = int(12 * teta_hour_avg / (2 * np.pi))
+
+        else:
+            # We compare the distance between the two detected begining and
+            # ending points for sec and min.
+            # The distance separating the sec should be inferior to the one
+            # separating the min. If not, the hour bar is on the sec bar.
+            d_sec = (y_s_circle[begin_sec] - y_s_circle[end_sec]) ** 2 + \
+                    (x_s_circle[begin_sec] - x_s_circle[end_sec]) ** 2
+
+            d_min = (y_s_circle[begin_min] - y_s_circle[end_min]) ** 2 + \
+                    (x_s_circle[begin_min] - x_s_circle[end_min]) ** 2
+
+            if d_sec > 0.5 * d_min:
+                begin_hour, end_hour = begin_sec, end_sec
+                avg_hour = int((end_hour + begin_hour) / 2)
+                avg_hour = 2 * avg_hour - avg_sec
+                teta_hour_avg = 2 * np.pi - teta[avg_hour]
+                time[0] = int(12 * teta_hour_avg / (2 * np.pi))
+            else:
+                begin_hour, end_hour = begin_min, end_min
+                avg_hour = int((end_hour + begin_hour) / 2)
+                avg_hour = 2 * avg_hour - avg_min
+                teta_hour_avg = 2 * np.pi - teta[avg_hour]
+                time[0] = int(12 * teta_hour_avg / (2 * np.pi))
+
+
+    #
+    #   Display the detected pixels
+    #
+
+    # # Uncomment the following lines to see where the different points are.
+    # fig = plt.figure()
+    # plt.imshow(img_gray_thresholded)
+    # plt.plot(y_l_circle, x_l_circle, color='black')
+    # plt.plot(y_s_circle, x_s_circle, color='black')
+    # plt.plot([center[1], center[1]], [0, img_gray_thresholded.shape[1]], color='black', linestyle='--')
+    # plt.plot([0, img_gray_thresholded.shape[0]], [center[0], center[0]], color='black', linestyle='--')
+    # plt.gray()
+    # radius = 5
+
+    # center = y_s_circle[begin_sec], x_s_circle[begin_sec]
+    # image.draw_circle(fig, center, radius, 'green')
+    # center = y_s_circle[end_sec], x_s_circle[end_sec]
+    # image.draw_circle(fig, center, radius, 'green')
+
+    # center = y_s_circle[begin_min], x_s_circle[begin_min]
+    # image.draw_circle(fig, center, radius, 'blue')
+    # center = y_s_circle[end_min], x_s_circle[end_min]
+    # image.draw_circle(fig, center, radius, 'blue')
+
+    # center = y_s_circle[begin_hour], x_s_circle[begin_hour]
+    # image.draw_circle(fig, center, radius, 'red')
+    # center = y_s_circle[end_hour], x_s_circle[end_hour]
+    # image.draw_circle(fig, center, radius, 'red')
+
+    return time
+
+## --------------------  Main Program  -------------------
+
+precision = 0.95
+
+def main():
+    """Main program"""
+    img_path = input('Give the path to your image :\n')
+    img = misc.imread(img_path)
+    img_gray_thresholded = threshold_image(img)
+    clean_thresholded_image(img_gray_thresholded, precision)
+    center, radius = find_center_radius(img_gray_thresholded, precision)
+    t = time(img, center, radius)
+    time_str = str(t[0]) + 'h' + str(t[1]) + 'm' + str(t[2]) + 's'
+    print('Hello Mister Mery, the time is', time_str)
+
+main()
+
+## --------------------  Test  -------------------
+
+# img1 = misc.imread('clock2015/clock_01.png')
+# img2 = misc.imread('clock2015/clock_02.png')
+# img3 = misc.imread('clock2015/clock_03.png')
+# img4 = misc.imread('clock2015/clock_04.png')
+# img5 = misc.imread('clock2015/clock_05.png')
+# img6 = misc.imread('clock2015/clock_06.png')
+# img7 = misc.imread('clock2015/clock_07.png')
+
+# img_gray_thresholded1 = threshold_image(img1)
+# img_gray_thresholded2 = threshold_image(img2)
+# img_gray_thresholded3 = threshold_image(img3)
+# img_gray_thresholded4 = threshold_image(img4)
+# img_gray_thresholded5 = threshold_image(img5)
+# img_gray_thresholded6 = threshold_image(img6)
+# img_gray_thresholded7 = threshold_image(img7)
+
+# clean_thresholded_image(img_gray_thresholded1, precision)
+# clean_thresholded_image(img_gray_thresholded2, precision)
+# clean_thresholded_image(img_gray_thresholded3, precision)
+# clean_thresholded_image(img_gray_thresholded4, precision)
+# clean_thresholded_image(img_gray_thresholded5, precision)
+# clean_thresholded_image(img_gray_thresholded6, precision)
+# clean_thresholded_image(img_gray_thresholded7, precision)
+
+# center1, radius1 = find_center_radius(img_gray_thresholded1, precision)
+# print(time(img1, center1, radius1))
+# plt.savefig('figures/center/center_img1')
+# center2, radius2 = find_center_radius(img_gray_thresholded2, precision)
+# print(time(img2, center2, radius2))
+# plt.savefig('figures/center/center_img2')
+# center3, radius3 = find_center_radius(img_gray_thresholded3, precision)
+# print(time(img3, center3, radius3))
+# plt.savefig('figures/center/center_img3')
+# center4, radius4 = find_center_radius(img_gray_thresholded4, precision)
+# print(time(img4, center4, radius4))
+# plt.savefig('figures/center/center_img4')
+# center5, radius5 = find_center_radius(img_gray_thresholded5, precision)
+# print(time(img5, center5, radius5))
+# plt.savefig('figures/center/center_img5')
+# center6, radius6 = find_center_radius(img_gray_thresholded6, precision)
+# print(time(img6, center6, radius6))
+# plt.savefig('figures/center/center_img6')
+# center7, radius7 = find_center_radius(img_gray_thresholded7, precision)
+# print(time(img7, center7, radius7))
+# plt.savefig('figures/center/center_img7')
+
+
+# # Display the plots, press enter to close all of them at once
+# plt.show(block=False)
+# input("Hit Enter To Close")
+# plt.close()
+
+
+## --------------------  End of Homework  -------------------
